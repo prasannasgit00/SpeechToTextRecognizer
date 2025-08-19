2f75a8dc19bc69f7410bc29efacf250d6812d7a1
prasannasgit00 <prasannapatgar53@gmail.com>
2025-08-20 02:59:26 +0530

    Add files via upload

---
+++ SpeechToTextRecognizer/Speech_To_Text/mygitproject.py
@@ -0,0 +1,1 @@
+print("prasannasgit00")
---
+++ SpeechToTextRecognizer/Speech_To_Text/speech.py
@@ -0,0 +1,45 @@
+import random
+import time
+
+import speech_recognition as sr
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+    guess = recognize_speech_from_mic(recognizer, microphone)
+    print("You said: {}".format(guess["transcription"]))
